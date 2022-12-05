59a5a9006267c1a777ecf93acd22d7ee71cd7488
IreneOliveto <106282460+IreneOliveto@users.noreply.github.com>
2022-12-05 16:32:04 +0100

    loading status (#2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     component: HomepageComponent
   },
   {
-    path: 'recipe',
+    path: 'recipes',
     component: RecipeComponent
   },
   {
--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Recipe } from 'src/app/models/recipe.model';
-// import { loadingRecipes } from 'src/app/state/actions/recipes.action';
+import { loadingRecipes } from 'src/app/state/actions/recipes.action';
 import { AppState } from 'src/app/state/app.state';
 
 
@@ -12,24 +12,20 @@ import { AppState } from 'src/app/state/app.state';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  // loading$: Observable<boolean>;
-  // recipes$: Observable<Recipe[]>;
+  loading$: Observable<boolean>;
+  recipes$: Observable<Recipe[]>;
 
-  // constructor(
-  //   private store: Store<AppState>
-  // ) {
-  //   this.loading$ = new Observable<boolean>();
-  //   this.recipes$ = new Observable<Recipe[]>();
-  // }
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.loading$ = new Observable<boolean>();
+    this.recipes$ = new Observable<Recipe[]>();
+  }
 
   ngOnInit(): void {
-    // // Check the loading state
-    // this.loading$ = this.store.select(state => state.recipeStates.loading);
-    // // Check the apartments state
-    // this.recipes$ = this.store.select(state => state.recipeStates.recipes);
-
-    // // Trigger loadingApartment actions. Loading should be passing from false => true
-    // this.store.dispatch(loadingRecipes());
+    this.loading$ = this.store.select(state => state.recipeStates.loading);
+    this.recipes$ = this.store.select(state => state.recipeStates.recipes);
+    this.store.dispatch(loadingRecipes());
 
   }
 }
